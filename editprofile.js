e7188ad15c34c861f73b027944316fbc995dbcfa
Ricky Hadi <ricky.hadi@mvig.net>
2018-02-22 20:19:17 +0700

    init

---
+++ editprofile.js
@@ -0,0 +1,33 @@
+function editprofile(appConfig, renderer, helper){
+	var express = require('express');
+	var router = express.Router();
+
+	router.get('/', function(req, res, next) {
+	  var section = '/EDITPROFILE';
+	  if(!helper.isLogin(req)){
+		res.status(403);
+		section = '/ERROR/403';
+	  }
+	  var content = renderer.renderPage(section, req)
+	  .then(function(content){
+		res.send(content);
+	  });
+	});
+
+	router.post('/', function(req, res, next) {
+	  if(helper.isLogin(req)){
+		try{
+
+		  res.send('00|Success');
+		}
+		catch(ex){
+		  console.log(ex);
+		  res.send('00|Success');
+		}
+	  }
+	  res.send('403|Forbidden');
+	});
+	
+	return router;
+}
+module.exports = router;
